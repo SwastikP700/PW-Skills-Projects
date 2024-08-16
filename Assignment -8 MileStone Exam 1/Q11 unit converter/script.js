8f9cc8aded0652e39eb2fb127c6b337c245a2d4f
Swastik Pattanaik <123298295+SwastikP700@users.noreply.github.com>
2024-08-16 16:06:22 +0530

    Add files via upload

---
+++ Assignment -8 MileStone Exam 1/Q11 unit converter/script.js
@@ -0,0 +1,14 @@
+function ctof() {
+    const celcius = document.getElementById('celcius').value;
+    const result = document.getElementById('result');
+    if (celcius==="") {
+        alert("Invalid Temprature Input")
+    }
+    else {
+    console.log(result);
+    const falenheit = (celcius * 9 / 5) + 32;
+    result.innerText = falenheit;
+    }
+};
+
+document.getElementById('btn').addEventListener("click", ctof);
